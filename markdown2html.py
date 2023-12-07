5c8f08b83c18f1f9ceea928a2f9098398df6d083
hug0-cstrs <hugo.casteras@outlook.fr>
2023-12-07 11:40:17 +0100

    add documentation

--- markdown2html.py
+++ markdown2html.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Write a script markdown2html.py that takes an argument 2 strings:
+First argument is the name of the Markdown file
+Second argument is the output file name
+"""
 import sys
 import os
 import markdown
